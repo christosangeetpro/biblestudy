6ad46fd41f17433e895f474df15af883f432ca1a
Christo Sangeet Pro <50114338+christosangeetpro@users.noreply.github.com>
2026-02-28 12:54:43 +0600

    Add files via upload

---
+++ book_bible_study/new_testament.js
@@ -0,0 +1,218 @@
+[
+  {
+    "new_testament": "Matthew",
+    "bn_name": "মথি",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Mark",
+    "bn_name": "মার্ক",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Luke",
+    "bn_name": "লূক",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "John",
+    "bn_name": "যোহন",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Acts",
+    "bn_name": "প্রেরিতদের কার্য",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Romans",
+    "bn_name": "রোমীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "1_Corinthians",
+    "bn_name": "১ করিন্থীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "2_Corinthians",
+    "bn_name": "২ করিন্থীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Galatians",
+    "bn_name": "গালাতীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Ephesians",
+    "bn_name": "ইফিষীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Philippians",
+    "bn_name": "ফিলিপীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Colossians",
+    "bn_name": "কলসীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "1_Thessalonians",
+    "bn_name": "১ থিষলনীকীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "2_Thessalonians",
+    "bn_name": "২ থিষলনীকীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "1_Timothy",
+    "bn_name": "১ তীমথিয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "2_Timothy",
+    "bn_name": "২ তীমথিয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Titus",
+    "bn_name": "তীত",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Philemon",
+    "bn_name": "ফিলীমন",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Hebrews",
+    "bn_name": "ইব্রীয়",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "James",
+    "bn_name": "যাকোব",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "1_Peter",
+    "bn_name": "১ পিতর",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "2_Peter",
+    "bn_name": "২ পিতর",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "1_John",
+    "bn_name": "১ যোহন",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "2_John",
+    "bn_name": "২ যোহন",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "3_John",
+    "bn_name": "৩ যোহন",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Jude",
+    "bn_name": "যিহূদা",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  },
+  {
+    "new_testament": "Revelation",
+    "bn_name": "প্রকাশিত বাক্য",
+    "three_seconds_summary": "বিস্তারিত",
+    "three_minutes_summary": "বিস্তারিত",
+    "thirty_minutes_summary": "বিস্তারিত",
+    "detailed_study": "বিস্তারিত"
+  }
+]
